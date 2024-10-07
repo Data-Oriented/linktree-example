eb709710bd1e1db1ef0f6ec792b3810eef8492c6
Afilal <mafilal@gmail.com>
2024-10-07 14:56:51 -0600

    Added JS file

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-function cal() {
-    var a = parseInt(document.querySelector("#value1").value);
-    var b = parseInt(document.querySelector("#value2").value);
-    var op = document.querySelector("#operator").value;
-    var calculate;
+document.getElementById("send").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevents the default form submission behavior
 
-    if (op == "add") {
-        calculate = a + b;
-    } else if (op == "min") {
-        calculate = a - b;
-    } else if (op == "mul") {
-        calculate = a * b;
-    } else if (op == "div") {
-        calculate = a / b;
-    }
-    console.log(calculate);
-    return calculate;
-}
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    console.log("Name:", name);
+    console.log("Phone:", phone);
+    console.log("Email:", email);
+    console.log("Message:", message);
+    alert("Your form has been submitted!");
+    location.reload();
+
+
+});
+
