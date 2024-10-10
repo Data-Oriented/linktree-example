b12628e3cf6b0250f3475750f5f00baae2f1df37
Afilal <mafilal@gmail.com>
2024-10-09 20:21:28 -0600

    updated to dyanmic IDs for radio buttons

--- main.js
+++ main.js
@@ -5,11 +5,6 @@ document.getElementById("send").addEventListener("click", function(event) {
     let phone = document.getElementById("phone").value;
     let email = document.getElementById("email").value;
     let message = document.getElementById("message").value;
-    let html = document.getElementById("html").value;
-    let css = document.getElementById("css").value;
-    let javascript = document.getElementById("javascript").value;
-    let python = document.getElementById("python").value;
-    
     
     let selectedSkill = document.querySelector('input[name="skill"]:checked');
     
@@ -23,45 +18,45 @@ document.getElementById("send").addEventListener("click", function(event) {
     } else {
         console.log("No skill selected");
     }
+    
     alert("Your form has been submitted!");
 
-        // Construct mailto link
-        let mailtoLink = `mailto:mafilal@gmail.com?subject=Form Submission&body=Name: ${name}%0APhone: ${phone}%0AEmail: ${email}%0AMessage: ${message}%0ASkill: ${selectedSkill}`;
+    // Construct mailto link
+    let skillValue = selectedSkill ? selectedSkill.value : "No skill selected";
+    let mailtoLink = `mailto:mafilal@gmail.com?subject=Form Submission&body=Name: ${name}%0APhone: ${phone}%0AEmail: ${email}%0AMessage: ${message}%0ASkill: ${skillValue}`;
 
-        // Open the mailto link
-        window.location.href = mailtoLink;
+    // Open the mailto link
+    window.location.href = mailtoLink;
 });
 
 document.getElementById("plusBtn").addEventListener("click", function(event) {
     for (let i=0; i<3; i++) {
-        here.insertAdjacentHTML('beforeend', `
+        document.getElementById('here').insertAdjacentHTML('beforeend', `
             <div class="group" id="skillsButton">
-                <form>
-                    <p>Skills required for this project: </p>
-                    
-                    <label for="html">
-                        <input type="radio" id="html" name="skill" value="HTML">
-                        HTML
-                    </label>
-                    <br>
-                    
-                    <label for="css">
-                        <input type="radio" id="css" name="skill" value="CSS">
-                        CSS
-                    </label>
-                    <br>
-                    
-                    <label for="javascript">
-                        <input type="radio" id="javascript" name="skill" value="JavaScript">
-                        JavaScript
-                    </label>
-                    <br>
-                    
-                    <label for="python">
-                        <input type="radio" id="python" name="skill" value="Python">
-                        Python
-                    </label>
-                </form>
+                <p>Skills required for this project:</p>
+                
+                <label for="html_${i}">
+                    <input type="radio" id="html_${i}" name="skill" value="HTML">
+                    HTML
+                </label>
+                <br>
+                
+                <label for="css_${i}">
+                    <input type="radio" id="css_${i}" name="skill" value="CSS">
+                    CSS
+                </label>
+                <br>
+                
+                <label for="javascript_${i}">
+                    <input type="radio" id="javascript_${i}" name="skill" value="JavaScript">
+                    JavaScript
+                </label>
+                <br>
+                
+                <label for="python_${i}">
+                    <input type="radio" id="python_${i}" name="skill" value="Python">
+                    Python
+                </label>
             </div>
         `);    
     }
